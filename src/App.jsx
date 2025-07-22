27e973b01afc1d1a822e60c21e3fbba635044240
Ahmed Bestun <zeroaiko@live.com>
2025-07-22 20:33:19 +0200

    added conditional rendering for navbar and sidebar to not render on the authentication pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navigation from "./components/navigation/Navigation.jsx";
+import Sidebar from "./components/sidebar/Sidebar.jsx";
+import {Route, Routes, useLocation} from "react-router-dom";
+import LoginPage from "./pages/loginPage/LoginPage.jsx";
+import HomePage from "./pages/homePage/HomePage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const location = useLocation();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const authPages = ['/signin', '/signup'];
+    const isAuthPage = authPages.includes(location.pathname);
+
+    if (isAuthPage) {
+        return (
+            <div className="auth-layout">
+                <Routes>
+                    <Route path="/signin" element={<LoginPage />} />
+                </Routes>
+            </div>
+        );
+    }
+
+    return (
+        <div className="app-layout">
+            <Navigation />
+            <div className="main-container">
+                <Sidebar />
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App
